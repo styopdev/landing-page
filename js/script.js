f1bb8cf7fb642618ad92831dff7b14c93b6c5b93
styopdev <styopdev@hotmail.com>
2019-01-17 13:37:46 +0700

    Multiple fixes

--- js/script.js
+++ js/script.js
@@ -7,4 +7,24 @@ $( document ).ready(function() {
         items: 1,
     });
 
+    var windowWidth = $(window).width();
+
+    if(windowWidth > 992){
+        $(".navigationMobile").hide();
+    }
+
+    $(window).on('resize', function () {
+
+        var windowWidthResized = $(window).width();
+        var navigationIsVisible = $(".navigation").length;
+
+        if(windowWidthResized > 992 || navigationIsVisible > 0){
+            $(".navigationMobile").hide();
+        }
+    });
+
+    $(".responsiveBtn").on("click", function () {
+
+        $(".navigationMobile").slideToggle();
+    });
 });
